19d3f965ac24e7c8d4f511ba9a9bca250eed83b1
Jan Philipp Behrens <80356280+J-Behrens@users.noreply.github.com>
2022-07-27 15:35:18 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,8 +1,9 @@
 # TODO:	- enable generation of multiple instances in folder structure
 #		- include number of vertices in header of maze instances
 #		- in case of multiple generations of instances with same parameters, change randomness and append number to filename
-#		- test if instance is SAT with mif encoding and enabling via argument
+#		- test if instance is SAT with mif encoding and enabling via argument (maybe also add min. horizon to instance)
 #		- enable generation of same instance with variations of agent numbers
+#		- get length of shortest paths for each agent and append to instance
 
 import argparse, os
 from subprocess import getoutput
@@ -19,19 +20,19 @@ args = parser.parse_args()
 
 if args.maze:
 	instanceFileName  = 'maze_s' + args.size + '_a' + args.agents +'.lp'
-	instance_unfilled = getoutput('clingo maze.lp -c w=' + args.size + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1') 
+	instance_unfilled = getoutput('clingo encodings/maze.lp -c w=' + args.size + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1') 
 	instanceHeader    = '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n%\n% Grid Size X:\t\t\t' + str(int(args.size)*2) + '\n% Grid Size Y:\t\t\t' + str(int(args.size)*2) + '\n% Number of Agents:\t\t' + args.agents + '\n%\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n#program base.\n\n'
 
 if args.random:
 	instanceFileName  = 'random_s' + args.size + '_a' + args.agents + '_c' + str(args.cover) +'.lp'
 	numVertices       = str(int(((int(args.size)*int(args.size))/100)*args.cover))
-	instance_unfilled = getoutput('clingo random.lp -c w=' + args.size + ' -c v=' + numVertices + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
+	instance_unfilled = getoutput('clingo encodings/random.lp -c w=' + args.size + ' -c v=' + numVertices + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
 	instanceHeader    = '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n%\n% Grid Size X:\t\t\t\t' + args.size + '\n% Grid Size Y:\t\t\t\t' + args.size + '\n% Possible vertices used (in %):\t' + str(args.cover) + '\n% Number of Vertices:\t\t\t' + numVertices + '\n% Number of Agents:\t\t\t' + args.agents + '\n%\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n#program base.\n\n'
 
 with open(instanceFileName, "w") as instance:
 	instance.write(instance_unfilled)
 	
-instance_filled = getoutput('clingo ' + instanceFileName + ' task2.lp -c r=' + args.agents + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
+instance_filled = getoutput('clingo ' + instanceFileName + ' encodings/task.lp -c r=' + args.agents + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
 
 with open(instanceFileName, "w") as instance:
 	instance.write(instanceHeader)
@@ -39,4 +40,4 @@ with open(instanceFileName, "w") as instance:
 with open(instanceFileName, "a") as instance:
 	instance.write(instance_filled)
 	
-if args.visualize: os.system('clingo ' + instanceFileName + ' mif_to_asprilo.lp | viz')
+if args.visualize: os.system('clingo ' + instanceFileName + ' encodings/mif_to_asprilo.lp | viz')
