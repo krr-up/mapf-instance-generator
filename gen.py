b5e63cb4d2a4fe8d6806e868c373b243c8b18460
Jan Philipp Behrens <80356280+J-Behrens@users.noreply.github.com>
2022-08-09 22:41:05 +0200

    added option to add meta infos (for now just horizon)

--- gen.py
+++ gen.py
@@ -1,16 +1,15 @@
 #TODO:	- enable generation of multiple instances in folder structure
 #	- in case of multiple generations of instances with same parameters, change randomness and append number to filename instead of overwriting
-#	- test if instance is SAT with mif encoding, allow enabling via argument and add min. horizon to instance as fact
 #	- get more meta information like shortest paths and their length and add them as facts
-#	- enable generation of same instance with variations of agent numbers
-#	- get length of shortest paths for each agent and append to instance
+#	- enable generation of same instance with variations of agent numbers and/or minimum start-to-goal-distance
+#	- replace getoutput calls by using python clingo module and multishot for getting horizon to improve performance
 
 import argparse, os
 from subprocess import getoutput
 
 def agents():
 	global instance_filled
-	instance_filled = getoutput('clingo ' + instanceFileName + ' encodings/task.lp -c a=' + args.agents + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
+	instance_filled = getoutput('clingo ' + instanceFileName + ' encodings/agents.lp -c a=' + args.agents + ' --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
 
 def header():
 	header = '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n%\n% Size X and Y:\t\t' + str(int(args.size)*2) + '\n% Number of Agents:\t\t' + args.agents
@@ -37,6 +36,13 @@ def room():
 	instanceFileName  = 'room_s' + args.size + '_a' + args.agents + '_w' + str(args.width) +'.lp'
 	instance_unfilled = getoutput('clingo encodings/room.lp -c s=' + args.size + ' -c w=' + args.width + ' --rand-freq=1 --configuration=frumpy -V0 --out-atomf=%s. --out-ifs="\n" | head -n -1')
 
+def meta():
+	global instanceFileName
+	horizon=1
+	while 'UN' in getoutput('clingo ' + instanceFileName + ' encodings/mif.lp -c horizon=' + str(horizon) + ' -V0 -q'): horizon = horizon + 1
+	write('a', '\nmakespan(' + str(horizon) + ').')
+	os.rename(instanceFileName, instanceFileName[:-3] + '_h' + str(horizon) +'.lp')
+
 def write(mode, string):
 	with open(instanceFileName, mode) as instance:
 		instance.write(string)
@@ -47,7 +53,7 @@ room_group   = parser.add_argument_group('required arguments for room')
 random_group = parser.add_argument_group('required arguments for random')
 parser.add_argument(      'type',              help='type of instance to be generated',         choices=('maze', 'random', 'room'))
 parser.add_argument(      "-v", "--visualize", help="convert to and visualize with asprilo",    action='store_true')
-parser.add_argument(      "-t", "--test",      help="test if instance is solvable",             action='store_true')
+parser.add_argument(      "-m", "--meta",      help="gets and adds meta informations",          action='store_true')
 req_group.add_argument(   "-s", "--size",      help="size of instance",               type=str, required=True)
 req_group.add_argument(   "-a", "--agents",    help="number of agents",               type=str, required=True)
 room_group.add_argument(  "-w", "--width",     help="width of rooms",                 type=str)
@@ -61,4 +67,5 @@ write('w', instance_unfilled)
 agents()
 write('w', header())
 write('a', instance_filled)
+if args.meta: meta()
 if args.visualize: os.system('clingo ' + instanceFileName + ' encodings/mif_to_asprilo.lp | viz')
