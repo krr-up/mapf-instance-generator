c85dd0a0c0617e863423caecf6a9a293005cd389
Jan Philipp Behrens <80356280+J-Behrens@users.noreply.github.com>
2023-11-01 20:52:26 +0100

    reachability, suffix, splitted meta options
    - Finding makespan much faster with reachability encoding
    - New option --suffix
    - Splitted the meta options (now: --makespan, --shortest-paths, --acyclicity)

--- gen.py
+++ gen.py
@@ -6,6 +6,24 @@ import cv2
 from subprocess import getoutput
 from random import randint, shuffle
 import numpy as np
+from mapf_soc import SocMAPF
+
+
+def acyclicity_check(timeout):
+    global horizon
+    if args.durations:
+            acyc_out = getoutput(f'clingo encodings/mif_dur.lp -c acyclic=1 -c horizon={horizon} {instanceFileName} --time-limit={str(round(timeout))}')
+    else:
+            acyc_out = getoutput(f'clingo encodings/mif.lp -c acyclic=1 -c horizon={horizon} {instanceFileName} --time-limit={str(round(timeout))}')
+    if ((timeout) < 0) or ('INTERRUPTED' in acyc_out):
+            clean_up('\nTIMEOUT')
+    elif 'UN' in acyc_out:
+            acyclic = ''
+    else:
+            acyclic = 'acyclic.'
+
+    with open(instanceFileName[:-3] + '_meta.lp', 'w') as metaFile:
+        metaFile.write(f'% meta information:\n#const horizon={horizon}.\nmakespan(horizon).\n{acyclic}')
 
 
 def add_agents(timeout):
@@ -63,11 +81,11 @@ def create_instance(timeout):
     global instanceFileName, instance_unfilled
 
     if args.type == 'maze':
-        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}.lp'
+        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}{args.suffix}.lp'
     elif args.type == 'random':
-        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}_c{args.cover}.lp'
+        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}_c{args.cover}{args.suffix}.lp'
     elif args.type in ['room', 'warehouse']:
-        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}_w{str(args.width)}.lp'
+        instanceFileName = f'{args.type}_x{args.size[0]}_y{args.size[1]}_a{args.agents}_w{str(args.width)}{args.suffix}.lp'
 
     if args.type == 'maze':
         instance_unfilled = getoutput(f'clingo encodings/{args.type}.lp -c x={args.size[0]} -c y={args.size[1]}                                      --sign-def=rnd --init-watches=rnd --rand-freq=1 -V0 --out-atomf=%s. --out-ifs="\n" --time-limit={str(timeout)} | head -n -1')
@@ -96,11 +114,30 @@ def add_durations(timeout):
 def get_horizon(timeout):
     '''Finds the minimal makespan (horizon) of an instance'''
     global instanceFileName
-    time_elapsed_meta = 0
-    time_start_meta = time.time()
+    global horizon
+    #time_elapsed_meta = 0
+    #time_start_meta = time.time()
     if args.durations:
         solution = getoutput(f'clingo encodings/mif_inc_dur.lp {instanceFileName} -W none --outf=1 --time-limit={str(round(timeout))}')
     else:
+        app = SocMAPF([instanceFileName],
+                      ['--warn=none'],
+                      "inertia",
+                      "makespan",
+                      False,
+                      False,
+                      False,
+                      lambda d:d+1,
+                      print_output = False
+                     )
+        app.main()
+        horizon = app.delta + app.min_horizon
+        
+        if not args.acyclicity:
+            with open(instanceFileName[:-3] + '_meta.lp', 'w') as metaFile:
+                metaFile.write(f'% meta information:\n#const horizon={horizon}.\nmakespan(horizon).')
+
+        '''
         solution = getoutput(f'clingo encodings/mif_inc.lp     {instanceFileName} -W none --outf=1 --time-limit={str(round(timeout))}')
     if (timeout < 0) or ('INTERRUPTED' in solution):
         clean_up('\nTIMEOUT')
@@ -109,23 +146,10 @@ def get_horizon(timeout):
     if 'Calls' in solution:
         solution = solution.split("Calls          : ", 1)[1]
         horizon = str(int(solution.split("% Time", 1)[0])-1)
+        '''
 
-        if args.durations:
-            acyc_out = getoutput(f'clingo encodings/mif_dur.lp -c acyclic=1 -c horizon={horizon} {instanceFileName} --time-limit={str(round(timeout-time_elapsed_meta))}')
-        else:
-            acyc_out = getoutput(f'clingo encodings/mif.lp -c acyclic=1 -c horizon={horizon} {instanceFileName} --time-limit={str(round(timeout-time_elapsed_meta))}')
-        if ((timeout-time_elapsed_meta) < 0) or ('INTERRUPTED' in acyc_out):
-            clean_up('\nTIMEOUT')
-        elif 'UN' in acyc_out:
-            acyclic = ''
-        else:
-            acyclic = 'acyclic.'
-
-        with open(instanceFileName[:-3] + '_meta.lp', 'w') as metaFile:
-            metaFile.write(f'% meta information:\n#const horizon={horizon}.\nmakespan(horizon).\n{acyclic}')
-
-    else:
-        print(solution)
+    #else:
+    #    print(solution)
 
 
 def get_shortest_paths(timeout):
@@ -164,7 +188,7 @@ def img2inst(timeout):
     instance_unfilled = ''
     if not args.agents:
         args.agents = '0'
-    instanceFileName = (args.type.split('/')[-1]).split('.')[0] + f'_x{args.size[0]}_y{args.size[1]}_a{args.agents}.lp'
+    instanceFileName = (args.type.split('/')[-1]).split('.')[0] + f'_x{args.size[0]}_y{args.size[1]}_a{args.agents}{args.suffix}.lp'
     image = cv2.imread(args.type, cv2.IMREAD_GRAYSCALE)
     resized_image = cv2.resize(image, (int(args.size[0]), int(args.size[1])),
                                interpolation=cv2.INTER_NEAREST)
@@ -228,9 +252,11 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT,  signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-parser = argparse.ArgumentParser(usage='python gen.py (maze | random -c [0-100] | room -w WIDTH | warehouse -w WIDTH) -s SIZE -a AGENTS [-d DISTANCE] [-dur MINDUR MAXDUR] [-m] [-t TIMEOUT] [-v] [-h] [-q]')
+parser = argparse.ArgumentParser(usage='python gen.py (maze | random -c [0-100] | room -w WIDTH | warehouse -w WIDTH) -s SIZE -a AGENTS [-d DISTANCE] [-dur MINDUR MAXDUR] [-m] [-suf SUFFIX] [-t TIMEOUT] [-v] [-h] [-q]')
 req_group = parser.add_argument_group('required arguments for all instance types')
 parser.add_argument('type', help='')
+parser.add_argument('-acyc', '--acyclicity', action='store_true',
+                    help='checks acyclicity')
 parser.add_argument('--allSPs', action='store_true',
                     help='generates all shortest paths instead of one')
 parser.add_argument('-d', '--distance', type=str, default='0',
@@ -238,10 +264,14 @@ parser.add_argument('-d', '--distance', type=str, default='0',
 parser.add_argument('-dur', '--durations', nargs=2,
                     metavar=('MINDUR', 'MAXDUR'),
                     type=str, help='generates instances with durations')
-parser.add_argument('-m', '--meta', help='gets and adds meta informations',
+parser.add_argument('-m', '--makespan', help='searches for minimal makespan',
                     action='store_true')
 parser.add_argument('-q', '--quiet', help='turns on quiet mode',
                     action='store_true')
+parser.add_argument('-sp', '--shortest_paths', action='store_true',
+                    help='searches for shortest paths')
+parser.add_argument('-suf', '--suffix', help='appends suffix to filename',
+                    type=str, default='')
 parser.add_argument('-t', '--timeout', help='sets a timeout in seconds',
                     type=str, default='0')
 parser.add_argument('-v', '--visualize', action='store_true',
@@ -263,11 +293,10 @@ if '.lp' not in args.type and not args.size:
     raise parser.error('--size required')
 
 if '.lp' in args.type:
-    instanceFileName = (args.type.split('/')[-1])
+    instanceFileName = args.type.split('/')[-1].replace('.lp', f'{args.suffix}.lp')
     if args.agents:
         clean_instance(instanceFileName)
-    if args.agents:
-        instanceFileName = instanceFileName.split('_a')[0] + f'_a{args.agents}.lp'
+        instanceFileName = instanceFileName.split('_a')[0] + f'_a{args.agents}{args.suffix}.lp'
 
 if '.' not in args.type and not args.agents:
     args.agents = '0'
@@ -289,10 +318,12 @@ if args.durations:
     run('add_durations')
 if '.' not in args.type:
     run('add_header')
-if args.meta:
+if args.shortest_paths:
     run('get_shortest_paths')
+if args.makespan:
     run('get_horizon')
-
+    if args.acyclicity:
+        run('acyclicity_check')
 if args.visualize:
     os.system(f'clingo {instanceFileName} encodings/mif_to_asprilo.lp | viz')
 # Uses clingo to translate the instance from mif- to asprilo-format and opens it with the asprilo visualizer
